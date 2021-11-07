0cc23a624e93611f76950dae011623f39e1aad70
nikdimitriadis <dimitriadisnikos3@gmail.com>
2021-11-07 22:02:26 +0200

    newChange

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+const menu = document.querySelector(".menu");
+const burger1 = document.querySelector(".burger1");
+const burger2 = document.querySelector(".burger2");
+const burger3 = document.querySelector(".burger3");
 const rotate = document.querySelector(".navigation-chev");
 const ulHide = document.querySelector(".choices");
 
-rotate.addEventListener("click", () => {
+menu.addEventListener("click", () => {
+  burger1.classList.toggle("peristrofi1");
+  burger2.classList.toggle("peristrofi2");
+
+  burger3.classList.toggle("diagrafi");
+
   rotate.classList.toggle("rotated");
   ulHide.classList.toggle("hiden");
 });
-console.log(rotate);
